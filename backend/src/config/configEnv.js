6480cbb9a878412281e45bf4a06c8f708a94ee5f
iTzFireFoxX <juanarevaloduran@gmail.com>
2025-11-02 19:45:39 -0300

    Creacion index y configEnv

---
+++ backend/src/config/configEnv.js
@@ -0,0 +1,13 @@
+"use strict";
+import dotenv from "dotenv";
+
+
+dotenv.config();
+
+export const HOST = process.env.HOST || "localhost";
+export const PORT = process.env.PORT || 3000;
+export const DB_USERNAME = process.env.DB_USERNAME
+export const PASSWORD = process.env.PASSWORD;
+export const DATABASE = process.env.DATABASE;
+export const JWT_SECRET = process.env.JWT_SECRET;
+export const cookieKey = process.env.COOKIE_KEY;
