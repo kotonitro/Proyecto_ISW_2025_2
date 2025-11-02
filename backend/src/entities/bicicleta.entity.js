b51feee58c908bed61901d06b65cbd89db34adfe
matias-vilchesb <matias.75vilches@gmail.com>
2025-11-02 20:46:08 -0300

    arreglo espacios

--- backend/src/entities/bicicleta.entity.js
+++ backend/src/entities/bicicleta.entity.js
@@ -1,41 +1,40 @@
-import { EntitySchema } from "typeorm"
-
+import { EntitySchema } from "typeorm";
 
 export const Bicicleta = new EntitySchema({
-    name: "Bicicleta",
-    tableName: "bicicletas",
-    columns: {
-        id_bicicleta: {
-            primary: true,
-            type: "int",
-            generated: "increment",
+  name: "Bicicleta",
+  tableName: "bicicletas",
+  columns: {
+    id_bicicleta: {
+      primary: true,
+      type: "int",
+      generated: "increment",
+    },
+    id_usuario: {
+      type: "int",
+    },
+    marca: {
+      type: "varchar",
+      lenght: 255,
+      nullable: false,
+    },
+    modelo: {
+      type: "varchar",
+      lenght: 255,
+      nullable: false,
+    },
+    color: {
+      type: "varchar",
+      lenght: 255,
+      nullable: false,
+    },
+    relations: {
+      usuario: {
+        target: "Usuario",
+        type: "many-to-one",
+        joinColumn: {
+          name: "id_usuario",
         },
-        id_usuario: {
-            type: "int",
-        },
-        marca: {
-            type: "varchar",
-            lenght: 255,
-            nullable: false,
-        },
-        modelo: {
-            type: "varchar",
-            lenght: 255,
-            nullable: false,
-        },
-        color: {
-            type: "varchar",
-            lenght: 255,
-            nullable: false,
-        },
-        relations: {
-            usuario: { 
-                target: "Usuario", 
-                type: "many-to-one", 
-                joinColumn: {
-                    name: "id_usuario", 
-                }, 
-            },
-        },
-    }
-})
+      },
+    },
+  },
+});
