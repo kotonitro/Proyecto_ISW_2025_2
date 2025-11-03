744e7f5768ea8443df9b82977049bff9be4d7662
kithian <naitsirhc.padi@gmail.com>
2025-11-03 01:16:12 -0300

    Programing adds

---
+++ backend/src/routes/profile.routes.js
@@ -0,0 +1,2 @@
+import { Router } from "express";
+import { authMiddleware } from "../middleware/auth.middleware.js";
---
+++ backend/src/validation/auth.validation.js
@@ -0,0 +1,6 @@
+"use strict";
+import Joi from "joi";
+
+  export const variablequevamosausar = Joi.object({
+    
+    });
