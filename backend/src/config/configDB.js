21444fbada78952dbb8b13b37008862b4db017cd
iTzFireFoxX <juanarevaloduran@gmail.com>
2025-11-10 14:16:01 -0300

    Creacion ruta home

--- backend/src/config/configDB.js
+++ backend/src/config/configDB.js
@@ -5,21 +5,6 @@ import { DATABASE, DB_USERNAME, HOST, PASSWORD } from "./configEnv.js";
 export const AppDataSource = new DataSource({
   type: "postgres",
   host: `${HOST}`,
-<<<<<<< HEAD
-  port: `${PORT}`,
-  username: `${DB_USERNAME}`,
-  password: `${PASSWORD}`,
-  database: `${DATABASE}`,
-  entities: ["src/entity/*.js"],
-  synchronize: true,
-  logging: false,
-});
-
-export async function connectDb() {
-  try {
-    await AppDataSource.initialize();
-    console.log("Conexión exitosa con la base de datos");
-=======
   port: 5432,
   username: `${DB_USERNAME}`,
   password: `${PASSWORD}`,
@@ -33,13 +18,8 @@ export async function connectDB() {
   try {
     await AppDataSource.initialize();
     console.log("=> Conexión exitosa a la base de datos PostgreSQL!");
->>>>>>> dev
   } catch (error) {
     console.error("Error al conectar con la base de datos:", error);
     process.exit(1);
   }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> dev
