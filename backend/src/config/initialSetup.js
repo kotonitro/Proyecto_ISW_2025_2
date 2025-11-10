2223e754c02d5314eae9df0a820ee3002b8a1931
kotonitro <kotonitro554@gmail.com>
2025-11-10 15:01:08 -0300

    Creación de script admin

--- backend/src/config/initialSetup.js
+++ backend/src/config/initialSetup.js
@@ -1,5 +1,29 @@
-"use strict";
-import bcrypt from "bcrypt";
+import Encargado from "../entities/encargado.entity.js";
 import { AppDataSource } from "./configDB.js";
+import bcrypt from "bcrypt";
 
-//WIP -> yo creo que aqui deberíamos hacer que se cree el encargado con rol admin
+export async function createAdmin() {
+  try {
+    const userRepository = AppDataSource.getRepository(Encargado);
+    const count = await userRepository.count();
+    if (count > 0) return;
+
+    const now = new Date();
+
+    await Promise.all([
+      userRepository.save(
+        userRepository.create({
+          nombre: "Admin",
+          esAdmin: true,
+          rut: "12345678-9",
+          correo: "admin@example.com",
+          contrasena: await bcrypt.hash("admin", 10),
+          telefono: "12312312",
+        }),
+      ),
+    ]);
+    console.log("Admin creado satisfactoriamente");
+  } catch (error) {
+    console.error("Error al crear el administrador:", error);
+  }
+}
