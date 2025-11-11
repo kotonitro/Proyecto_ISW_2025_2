1461525853c1c4cc372972e2748f03b8c93a31b0
Jose H <josehormazabal296@gmail.com>
2025-11-11 00:25:53 -0300

    Creacion de entidad notificacion

---
+++ backend/src/entities/notificacion.entity.js
@@ -0,0 +1,40 @@
+import { EntitySchema } from "typeorm";
+
+export const Notificacion = new EntitySchema({
+  name: "Notificacion",
+  tableName: "notificaciones",
+  columns: {
+    id: {
+      primary: true,
+      type: "int",
+      generated: "increment",
+    },
+    mensaje: {
+      type: "varchar",
+      length: 255,
+      nullable: false,
+    },
+    bicicleteroId: {
+      type: "varchar", // O "int" si el ID es un número
+      nullable: false,
+      name: "bicicletero_id", // Nombre opcional en la BD
+    },
+    leida: {
+      type: "boolean",
+      default: false,
+      nullable: false,
+    },
+ 
+    fechaCreacion: {
+      type: "timestamp",
+      createDate: true,
+      name: "fecha_creacion",
+    },
+    
+    fechaActualizacion: {
+      type: "timestamp",
+      updateDate: true,
+      name: "fecha_actualizacion",
+    },
+  },
+});
