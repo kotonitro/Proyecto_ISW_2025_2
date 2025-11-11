d31b9976f19a959f17bec47e6edc6d671709c340
iTzFireFoxX <juanarevaloduran@gmail.com>
2025-11-11 20:48:03 -0300

    Endopint eliminar bicicletero

--- backend/src/routes/bicicletero.routes.js
+++ backend/src/routes/bicicletero.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { authMiddleware } from "../middleware/auth.middleware.js";
-import { handleCreateBicicletero } from "../controllers/bicicletero.controller.js";
+import { handleCreateBicicletero, handleDeleteBicicletero } from "../controllers/bicicletero.controller.js";
 
 const router = Router();
 router.use(authMiddleware);
 
 router.post("/", handleCreateBicicletero);
+router.delete("/", handleDeleteBicicletero);
 
 export default router;
