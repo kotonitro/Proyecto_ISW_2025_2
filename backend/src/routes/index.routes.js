4fcc53808c8c68e6daefaebb547b16074652f4e8
Jose H <josehormazabal296@gmail.com>
2025-11-11 23:20:08 -0300

    actualizacion index routes

--- backend/src/routes/index.routes.js
+++ backend/src/routes/index.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import authRoutes from "./auth.routes.js";
 import custodiaRoutes from "./custodia.routes.js";
-import bicicleteroRoutes from "./bicicletero.routes.js"
-import encargadoRoutes from "./encargado.routes.js"
+import bicicleteroRoutes from "./bicicletero.routes.js";
+import encargadoRoutes from "./encargado.routes.js";
+import notificacionRoutes from "./notificacion.routes.js";
 
 export function routerApi(app) {
   const router = Router();
@@ -12,4 +13,5 @@ export function routerApi(app) {
   router.use("/custodia", custodiaRoutes);
   router.use("/bicicleteros", bicicleteroRoutes);
   router.use("/encargados", encargadoRoutes);
+  app.use("/api/notificaciones", notificacionRoutes);
 }
