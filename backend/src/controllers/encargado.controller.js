bb0d06fa143408d6ee0cf75b6d4c3d3ad5e045f4
iTzFireFoxX <juanarevaloduran@gmail.com>
2025-11-12 02:33:39 -0300

    fix

--- backend/src/controllers/encargado.controller.js
+++ backend/src/controllers/encargado.controller.js
@@ -1,60 +1,31 @@
-import {
-  getEncargados,
-  createEncargado,
-} from "../services/encargado.service.js";
-import {
-  handleSuccess,
-  handleErrorClient,
-  handleErrorServer,
-} from "../handlers/responseHandlers.js";
+import { getEncargados, createEncargado } from "../services/encargado.service.js";
+import { handleSuccess, handleErrorClient, handleErrorServer } from "../handlers/responseHandlers.js";
 import { encargadoValidation } from "../validations/encargado.validation.js";
 
-export async function handleGetEncargados(req, res) {
-  try {
-    const Encargados = await getEncargados();
-    return handleSuccess(
-      res,
-      200,
-      "Encargados obtenidos correctamente",
-      Encargados,
-    );
-  } catch (error) {
-    return handleErrorServer(
-      res,
-      500,
-      "Error interno al obtener los encargados",
-      error.message,
-    );
-  }
+export async function handleGetEncargados(req, res){
+    try {
+        const Encargados = await getEncargados();
+        return handleSuccess(res, 200, "Encargados obtenidos correctamente", Encargados);
+    } catch (error) {
+        return handleErrorServer(res, 500, "Error interno al obtener los encargados", error.message);
+    }
 }
 
 export async function handleCreateEncargado(req, res) {
-  const encargadoData = req.body;
+    const encargadoData = req.body;
 
-  try {
-    const { error, value } = encargadoValidation.validate(encargadoData, {
-      abortEarly: false,
-    });
-    if (error) {
-      const errorDetails = error.details.map((detail) => ({
-        field: detail.context.key,
-        message: detail.message.replace(/['"]/g, ""),
-      }));
-      return handleErrorClient(
-        res,
-        400,
-        "Error de validacion en los datos.",
-        errorDetails,
-      );
+    try {
+        const { error, value } = encargadoValidation.validate(encargadoData, {abortEarly: false});
+        if (error){
+            const errorDetails = error.details.map((detail) => ({
+                field: detail.context.key,
+                message: detail.message.replace(/['"]/g, ""),
+            }));
+            return handleErrorClient(res, 400, "Error de validacion en los datos.", errorDetails,);
+        }
+        const newEncargado = await createEncargado(value);
+        handleSuccess(res, 201, "Encargado creado exitosamente", newEncargado);
+    } catch (error) {
+        handleErrorServer(res, 500, "Error interno al crear el encargado", error.message);
     }
-    const newEncargado = await createEncargado(value);
-    handleSuccess(res, 201, "Encargado creado exitosamente", newEncargado);
-  } catch (error) {
-    handleErrorServer(
-      res,
-      500,
-      "Error interno al crear el encargado",
-      error.message,
-    );
-  }
 }
