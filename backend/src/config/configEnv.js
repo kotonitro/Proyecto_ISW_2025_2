c11d57b81d99ba8b7d1d9e93925581f53b31ef0b
kotonitro <kotonitro554@gmail.com>
2025-12-29 05:44:22 -0300

    fix

--- backend/src/config/configEnv.js
+++ backend/src/config/configEnv.js
@@ -8,7 +8,7 @@ dotenv.config();
 export const HOST = process.env.HOST || "localhost";
 export const PORT = process.env.PORT || 3000;
 export const DB_USERNAME = process.env.DB_USERNAME;
-export const PASSWORD = process.env.PASSWORD;
+export const PASSWORD = process.env.DB_PASSWORD;
 export const DATABASE = process.env.DATABASE;
 export const JWT_SECRET = process.env.JWT_SECRET;
 export const cookieKey = process.env.COOKIE_KEY;
